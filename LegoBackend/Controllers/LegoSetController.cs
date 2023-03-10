a5a29ef7a72094ffc45ffaa64d65b87cd08ef889
Melanie <melanievandegraaf1@live.nl>
2023-03-10 21:25:18 +0100

    Added GetAll method for LegoSets (without themes)

---
+++ LegoBackend/Controllers/LegoSetController.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using LegoBackend.Model;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace LegoBackend.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class LegoSetController : Controller
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<LegoSetEntity>> GetAll()
+    {
+        using var connection = new MySqlConnection("Server=localhost;Database=legoapp;User=root;Password=root");
+        
+        return Ok(connection.Query<LegoSetEntity>("SELECT * FROM lego_set"));
+    }
+}
--- LegoBackend/Model/LegoSetEntity.cs
+++ LegoBackend/Model/LegoSetEntity.cs
@@ -5,9 +5,10 @@ public class LegoSetEntity
     public int Id { get; set; }
     public int TotalPieces { get; set; }
     public decimal Price { get; set; }
-    public float HeigthInCm { get; set; }
+    public float HeightInCm { get; set; }
     public float WidthInCm { get; set; }
     public float LengthInCm { get; set; }
-    public int ThemeId { get; set; }
-    public LegoThemeEntity Theme { get; set; } = new();
+    public int? ThemeId { get; set; }
+    public LegoThemeEntity? Theme { get; set; } = null;
+    public bool Retired { get; set; }
 }
